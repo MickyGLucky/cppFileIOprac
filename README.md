9b65ed72afe5a8d476bd8181e620aa2cc9543b61
MickyGLucky <ma0826@saskpolytech.ca>
2024-12-08 11:59:51 -0600

    tried to make the program more dynamic, practice write user's input to file and read them back to store in memory

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # cppFileIOprac
 A repository for practice how to use the fstream
+
+## Key syntax
+- Initialize an instance
+
+`std::fstream file_object`
+
+- Open a file_object
+
+`file_object.open(fileName,std::ios::in)` for write (input), `file_object.open(fileName,std::ios::in)` for read (output)
+
+`.open()` The `.open()` function in C++ is used to open a file with a specified mode (e.g., read, write) using an `std::fstream` object.
+`.is_open()`, `.fail()`, `.good()` to return a boolean to decice a file is open correctly.
+
+- Read file method.
+
+```cpp
+if (file_object.is_open())
+{
+    while (getline(file_to_read,line))
+    {
+		std::cout << line << std::endl;
+    }
+}
+else
+{
+	std::cout << "Errors" << std::endl;
+}
+
+```
+
+`.getline()` can be used to read data in the file one line at a time
+
+**Read method sample code snippet**
+```cpp
+if(!file_project)
+{
+    std::ceer << "error"<< std::endl;
+    return 1;
+}
+while(!file_project.eof())
+{
+    std::getline(file_project,line);
+    std::cout << line << std::endl;
+}
+file_project.close();
+
+```
+
+`.eof()`- returns true if the end of the file has been reached;
+`.close()` - close the file
+
+That's pretty much of this section.
--- fstreamPrac/fstreamPrac.cpp
+++ fstreamPrac/fstreamPrac.cpp
@@ -2,14 +2,20 @@
 #include <fstream>
 #include <string>
 #include <iomanip>
+#include <vector>
 
 int main()
 {
-    std::fstream file_to_write;
+    std::ofstream file_to_write;
 	std::string file_name ;
     std::string Msg;
+
+	std::string oline,line1, line2, line3;
+	std::vector<std::string> lines;
+
 	std::cout << "Enter the name of the file you want to write to: ";
     std::cin >> file_name;
+	std::cin.ignore(); // Clear the buffer newline;
     /*The code above will create a file with the name you provide and write the message ¡°Hello, I¡¯m writing to a file!¡± to it.
 The code will then print a message to the console to let you know if the file was written successfully or if there was an error.
 Reading from a file
@@ -19,9 +25,19 @@ The code below reads from a file and prints the contents to the console.*/
     file_to_write.open(file_name, std::ios::out);
     if (file_to_write.is_open())
     {
-		file_to_write << "Hello, I'm writing to a file!\n";
-        file_to_write << "This is the 2nd line!\n";
-        file_to_write << "This is the 3rd\n";
+		while (1)
+		{
+			std::cout << "Enter lines of text, enter a blank line to end: ";
+			std::getline(std::cin, oline);
+			file_to_write << oline << std::endl;
+			if (oline.empty())
+			{
+				std::cout << "Input is finished, writing to file..." << std::endl;
+				break;
+			}
+		}
+
+
         file_to_write.close();
 		Msg = "File written successfully";
 
@@ -35,7 +51,7 @@ The code below reads from a file and prints the contents to the console.*/
 	std::cout << Msg << std::endl;
     std::cout << std::setfill('-') << std::setw(50) << "-" << std::endl;
     /*Retrive the input by reading the file */
-	std::fstream file_to_read;
+	std::ifstream file_to_read;
 	std::string line;
 	std::cout << "Reading from file...begins" << std::endl;
     file_to_read.open(file_name, std::ios::in);
@@ -47,9 +63,11 @@ The code below reads from a file and prints the contents to the console.*/
     */
     if (file_to_read.is_open())
     {
+        
         std::cout << std::setfill('>') << std::setw(25) << ">" << std::endl;
         while (getline(file_to_read,line))
         {
+			lines.push_back(line);
 			std::cout << line << std::endl;
         }
 		std::cout << "File read successfully" << std::endl;
@@ -59,6 +77,13 @@ The code below reads from a file and prints the contents to the console.*/
 		std::cout << "Error opening file" << std::endl;
 	}
 	file_to_read.close();
+
+    std::cout << std::setfill('-') << std::setw(50) << "-" << std::endl;
+	// Check the lines are stored correctly
+	for (std::string l : lines) 
+	{
+		std::cout << l << std::endl;
+	}
 	return 0;
 
 }
